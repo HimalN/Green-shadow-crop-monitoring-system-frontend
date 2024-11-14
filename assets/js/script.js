13de8b177da6788eea1d73fab1e7d42785fa30cc
HimalN <himalnimsara88@gmail.com>
2024-11-15 00:17:13 +0530

    -->Signup Ui -->SPA

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("signup-lock").onclick = function() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("signup").style.display = "block";
+};
+
+document.getElementById("login-lock").onclick = function() {
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("login").style.display = "block";
+};
